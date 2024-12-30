3cd5f3ecd60900c7f495b2c696ebd78e8c5060a5
classicalJo <jb.barrientos@yahoo.com>
2024-12-29 22:13:39 -0300

    Fixes lint errors

--- src/main/modules/message/application/interfaces/message-adapter.interface.ts
+++ src/main/modules/message/application/interfaces/message-adapter.interface.ts
@@ -1,6 +1,4 @@
-import { MainMessages } from '@/shared/messages/main-messages.enum';
 import { RendererMessages } from '@/shared/messages/renderer-messages.enum';
-import { IMainPayloads } from '@/shared/payloads/main-payloads.interface';
 import { IRendererPayloads } from '@/shared/payloads/renderer-payloads.interface';
 
 export interface IHandlerAdapter {
