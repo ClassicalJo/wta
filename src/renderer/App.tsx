73b5ea1334e3babe62c7c3c0a4e792532fce3df8
classicalJo <jb.barrientos@yahoo.com>
2025-01-04 20:18:52 -0300

    Fixes format issues between typescript and prettier

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,23 +1,28 @@
 import React, { useEffect } from 'react';
 
 import { MainMessages } from '@/shared/messages/main-messages.enum';
-import './index.css'
-export default function App() {
-    useEffect(() => {
-        window.electron.onMainMessage(MainMessages.MAIN_MESSAGE, (payload) => {
-            console.log(payload);
-        });
-    }, []);
 
-    function handleClick() {
-        window.electron.rendererMessage('Hello from renderer');
-    }
-    return (
-        <>
-            <h1 className="text-3xl font-bold underline">
-                Hello world!
-            </h1>
-            <input className='bg-blue-500 rounded-full px-4 my-2 text-white' type='button' value='Send message' onClick={handleClick} />
-        </>
-    );
+import './index.css';
+
+export default function App() {
+  useEffect(() => {
+    window.electron.onMainMessage(MainMessages.MAIN_MESSAGE, (payload) => {
+      console.log(payload);
+    });
+  }, []);
+
+  function handleClick() {
+    window.electron.rendererMessage('Hello from renderer');
+  }
+  return (
+    <>
+      <h1 className='text-3xl font-bold underline'>Hello world!</h1>
+      <input
+        className='bg-blue-500 rounded-full px-4 my-2 text-white'
+        type='button'
+        value='Send message'
+        onClick={handleClick}
+      />
+    </>
+  );
 }
