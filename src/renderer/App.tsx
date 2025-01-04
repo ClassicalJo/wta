813c5fb20df1213da8a2af01e542122a2d47fa8f
classicalJo <jb.barrientos@yahoo.com>
2025-01-04 20:10:46 -0300

    Implements vite

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -1,21 +1,23 @@
 import React, { useEffect } from 'react';
 
 import { MainMessages } from '@/shared/messages/main-messages.enum';
-
+import './index.css'
 export default function App() {
-  useEffect(() => {
-    window.electron.onMainMessage(MainMessages.MAIN_MESSAGE, (payload) => {
-      console.log(payload);
-    });
-  }, []);
+    useEffect(() => {
+        window.electron.onMainMessage(MainMessages.MAIN_MESSAGE, (payload) => {
+            console.log(payload);
+        });
+    }, []);
 
-  function handleClick() {
-    window.electron.rendererMessage('Hello from renderer');
-  }
-  return (
-    <>
-      <div>Hello world!</div>
-      <input type='button' value='Send message' onClick={handleClick} />
-    </>
-  );
+    function handleClick() {
+        window.electron.rendererMessage('Hello from renderer');
+    }
+    return (
+        <>
+            <h1 className="text-3xl font-bold underline">
+                Hello world!
+            </h1>
+            <input className='bg-blue-500 rounded-full px-4 my-2 text-white' type='button' value='Send message' onClick={handleClick} />
+        </>
+    );
 }
