9dbdb631d2dff34ef1513e5ec2bc9022eabda655
classicalJo <jb.barrientos@yahoo.com>
2025-02-02 23:43:06 -0300

    Added custom font and styles to menu

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -11,7 +11,10 @@ import './index.css';
 export default function App() {
   const { starting, started, leaving, dispatchNavigation } =
     useDelayedNavigation(500);
-
+  const linkProps = {
+    isNavigating: leaving,
+    setIsNavigating: dispatchNavigation,
+  };
   const initialOpacity = starting ? 'opacity-0' : '';
   const startedOpacity = started ? 'opacity-100' : '';
   const leavingOpacity = leaving ? 'opacity-0' : '';
@@ -28,34 +31,37 @@ export default function App() {
   return (
     <div
       className={`
-        ${initialOpacity} transition-opacity duration-300 flex flex-1 items-center justify-center ${startedOpacity} ${leavingOpacity}     `}
+        ${initialOpacity} transition-opacity duration-300 flex flex-1 items-center justify-center ${startedOpacity} ${leavingOpacity}    `}
     >
       <MainBackground />
-      <div className='relative p-10 flex flex-col gap-4 underline font-bold bg-black/60  text-white border-white border-2 text-3xl'>
+      <div
+        className='relative p-8 flex flex-col gap-12 text-orange-100 text-6xl font-[Rock] 
+        '
+      >
         <DelayedLink
-          isNavigating={leaving}
-          setIsNavigating={dispatchNavigation}
+          {...linkProps}
+          className='hover:text-orange-400 transition-all duration-200 ease-in-out'
           to={'/character'}
         >
           Characters
         </DelayedLink>
         <DelayedLink
-          isNavigating={leaving}
-          setIsNavigating={dispatchNavigation}
+          {...linkProps}
+          className='hover:text-orange-400 transition-all duration-200 ease-in-out'
           to={'/gift'}
         >
           Gifts
         </DelayedLink>
         <DelayedLink
-          isNavigating={leaving}
-          setIsNavigating={dispatchNavigation}
+          {...linkProps}
+          className='hover:text-orange-400 transition-all duration-200 ease-in-out'
           to={'/ritual'}
         >
           Rituals
         </DelayedLink>
         <DelayedLink
-          isNavigating={leaving}
-          setIsNavigating={dispatchNavigation}
+          {...linkProps}
+          className='hover:text-orange-400 transition-all duration-200 ease-in-out'
           to={'/fight'}
         >
           Fight!
