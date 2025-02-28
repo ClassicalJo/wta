45bd40eb2ddb314102ab1c54b0046ef1eff9f4a3
classicalJo <jb.barrientos@yahoo.com>
2025-02-28 02:37:09 -0300

    Updates menu items and adds some details to Character, Gift and Main bgs

--- src/renderer/components/menu/MenuLink.tsx
+++ src/renderer/components/menu/MenuLink.tsx
@@ -6,7 +6,11 @@ export default function MenuLink(props: DelayedLinkProps) {
   return (
     <DelayedLink
       {...props}
-      className='px-6 py-4 text-center text-3xl tracking-widest text-white transition-all duration-150 ease-in-out hover:scale-110 hover:text-dark-text h-md:px-8 h-md:py-6 h-md:text-4xl h-lg:px-10 h-lg:py-8 h-lg:text-5xl h-xl:px-12 h-xl:py-10 h-xl:text-6xl'
-    />
+      className='relative max-w-md tracking-widest text-white transition-all duration-300 ease-in-out hover:translate-x-4 hover:scale-105'
+    >
+      <span className='bg-[linear-gradient(to_right,_#a1e7f5_0%,_#a1e7f5_50%,_#ffffff_50%,_#ffffff_100%)] bg-[length:200%_100%] bg-clip-text bg-[position:100%_0] text-transparent transition-all duration-300 ease-in-out hover:bg-[position:0%_0]'>
+        {props.children}
+      </span>
+    </DelayedLink>
   );
 }
